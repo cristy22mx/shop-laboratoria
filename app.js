ae8041a732aa9baa1bf67861ada6dfd5c7e9bcfc
carolbaam <carolinabaam@gmail.com>
2018-03-10 12:27:08 -0600

    subiendo otravez

---
+++ app.js
@@ -0,0 +1,86 @@
+const form=document.getElementById('search-form');
+const searchField=document.getElementById('search-key-word');
+const responseContainer=document.getElementById('response-container');
+/*
+const apiArtesania = () => {
+    fetch(`https://api.mercadolibre.com/sites/MLM/trends/search?category=MLM1574`)
+        .then(function(response) {
+            response.json().then(function(result) {
+                console.log(result);
+        });
+    })
+        .catch(function(err) {
+            console.log(err);
+        });
+};
+
+
+apiArtesania();
+*/
+
+/*
+const apiArtesania = () => {
+    fetch(`https://api.mercadolibre.com/users/226384143/`)
+        .then(function(response) {
+            response.json().then(function(result) {
+                console.log(result);
+        });
+    })
+        .catch(function(err) {
+            console.log(err);
+        });
+};
+
+apiArtesania();
+*/
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    responseContainer.innerHTML="";
+    searchedForText=searchField.value;
+   apiLoad();
+})
+const apiLoad = () => {
+    fetch(`https://api.mercadolibre.com/sites/MLM/search?q=${searchedForText}`, )
+        .then(function(response) {
+            response.json().then(function(result) {
+                console.log(result.results);
+                paintItems(result.results)
+
+        });
+    })
+        .catch(function(err) {
+            console.log(err);
+        });
+};
+
+const paintItems = (result) => {
+    
+    
+    let containerProducts = document.getElementById('site-container');
+    let templateProducts = ``;
+   
+     result.forEach((item) => {
+        
+        const addres=item.address.state_name;
+         const image=item.thumbnail;
+        templateProducts += `<div class="col s6 m3" data-url="">
+        <div class="card waves">
+            <div class= "card-content center-align circle">
+                <h1 > ${item.title}</h1>
+                <img class="responsive-img center" src="${image}">
+                <p>Price: ${item.price} MXN</p>
+                <p>Place: ${addres}</p>
+                <p>Available cuantity: ${item.available_quantity}</p>
+                <p>Seller status: ${item.seller.power_seller_status}</p>
+               
+            </div>
+        </div>`; 
+    
+//console.log(available);
+         
+     });
+
+     containerProducts.innerHTML = templateProducts;
+    
+}
+
