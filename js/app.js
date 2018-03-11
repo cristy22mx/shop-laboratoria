49c3df94ccb72838014b28b53dd25e6c0bd8a2f9
AnnKrn <anakrn.carreon@gmail.com>
2018-03-10 22:49:54 -0600

    maquetado simple

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
+$(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
 const form=document.getElementById('search-form');
 const searchField=document.getElementById('search-key-word');
-const responseContainer=document.getElementById('response-container');
+const responseContainer=document.getElementsByClassName('response-container');
 /*
 const apiArtesania = () => {
     fetch(`https://api.mercadolibre.com/sites/MLM/trends/search?category=MLM1574`)
@@ -63,18 +67,20 @@ const paintItems = (result) => {
         
         const addres=item.address.state_name;
          const image=item.thumbnail;
-        templateProducts += `<div class="col s6 m3" data-url="">
-        <div class="card waves">
-            <div class= "card-content center-align circle">
-                <h1 > ${item.title}</h1>
-                <img class="responsive-img center" src="${image}">
-                <p>Price: ${item.price} MXN</p>
-                <p>Place: ${addres}</p>
-                <p>Available cuantity: ${item.available_quantity}</p>
-                <p>Seller status: ${item.seller.power_seller_status}</p>
-               
+        templateProducts += `<div class="col s12 m3">
+        <div class="card">
+            <div class="card-image">
+                <img src="${image}">
+                <span class="card-title">${item.title}</span>
             </div>
-        </div>`; 
+            <div class="card-content">
+                <p>${item.price} MXN</p>
+            </div>
+            <div class="card-action">
+                <p href="#"></p>
+            </div>
+        </div>
+    </div>`
     
 //console.log(available);
          
