0309498415173195748113b696f4a5c8cee417da
Behtsa <behtsabet14@gmail.com>
2018-03-12 11:35:43 -0600

    template product

--- js/app.js
+++ js/app.js
@@ -8,22 +8,7 @@ const searchField=document.getElementById('search-key-word');
 const responseContainer=document.getElementsByClassName('response-container');
 const carCounter = document.getElementById('items-counter');
 let counter = 0;
-/*
-const apiArtesania = () => {
-    fetch(`https://api.mercadolibre.com/sites/MLM/trends/search?category=MLM1574`)
 
-        .then(function(response) {
-            response.json().then(function(result) {
-                console.log(result);
-        });
-    })
-        .catch(function(err) {
-            console.log(err);
-        });
-};
-
-
-booksCall();
 
 
 const musicCall = () => {
@@ -94,10 +79,48 @@ const apiLoad = () => {
         });
 };
 
-const increaseCounter = () => {
+const addToCar = (id, title, price) => {
+    let productsArray = [];
+    let templateProduct = ` `; 
+    templateProduct += `
+    <tr>
+        <td>
+            <div class="card horizontal">
+                <div class="card-image">
+                    <img src="https://lorempixel.com/100/190/nature/6">
+                </div>
+                <div class="card-stacked">
+                    <div class="card-content">
+                        <h5 class="card-title">${title}</h5>
+                        <p>Código del Artículo:</p>
+                        <p>Disponible:</p>
+                        <p>Cantidad:</p>
+                    </div>
+                    <div class="card-action">
+                        <a class="bnt" >Eliminar</a>
+                        <a class="bnt" >Editar</a>
+                    </div>
+                </div>
+            </div>
+        </td>   
+        <td class = "final-price right-align">${price}</td>
+    </tr>`
+
+    console.log(templateProduct);
+
+    let productDetails = templateProduct;
+
+    productsArray.push(productDetails);
+
+    localStorage.setItem('productDetails', JSON.stringify(productsArray));
+}
+
+const increaseCounter = (id, title, price) => {
   counter += 1;
   carCounter.innerText = counter;
   console.log(counter);
+  console.log(title, price);
+  addToCar(id, title, price);
 }
 
 const decreaseCounter = () => {
@@ -109,10 +132,13 @@ const decreaseCounter = () => {
 const changeButtonStatus = event => {
     let element = event.target
     let buttonText = element.firstChild.data;
+    let itemId = element.dataset.id;
+    let itemTitle = element.dataset.title;
+    let itemPrice = element.dataset.price;
 
     if(buttonText === "Agregar a carrito") {
         element.innerText = "Remover del carrito";
-        increaseCounter();
+        increaseCounter(itemId, itemTitle, itemPrice);
     } else {
         element.innerText = "Agregar a carrito";
         decreaseCounter();
@@ -120,15 +146,13 @@ const changeButtonStatus = event => {
 }
 
 const paintItems = (result) => {
-    
-    
     let containerProducts = document.getElementById('site-container');
     let templateProducts = ``;
    
      result.forEach((item) => {
-        
+        const id = item.id;
         const addres=item.address.state_name;
-         const image=item.thumbnail;
+        const image=item.thumbnail;
         templateProducts += `<div class="col s12 m3">
         <div class="card">
             <div class="card-image">
@@ -139,7 +163,8 @@ const paintItems = (result) => {
                 <p class="">${item.price} MXN</p>
             </div>
             <div class="card-action">
-                <button id="" onclick="changeButtonStatus(event)" class="btn waves-effect" type="" name="action">Agregar a carrito</button>
+                <button data-id="${id}" data-title="${item.title}" data-price="${item.price}" onclick="changeButtonStatus(event)" class="btn waves-effect" type="" name="action">Agregar a carrito</button>
+            }
             </div>
         </div>
     </div>`
